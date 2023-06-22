ca1e11d3a61994f83e5df01ccfdc9315f8237a7c
lahirujay99 <lahirujayawardana94@gmail.com>
2023-06-22 23:50:08 +0530

    clear all list functionalty done

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -19,6 +19,11 @@ export default function App() {
     );
   }
 
+  function handleDeleteAll() {
+    const c = window.confirm("Are you sure ?");
+    if (c === true) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -27,6 +32,7 @@ export default function App() {
         items={items}
         onDeleteItem={handlerDeleteItem}
         onUpdateItem={handleCheckItems}
+        onDeleteList={handleDeleteAll}
       />
       <Stats items={items} />
     </div>
@@ -79,11 +85,26 @@ function Form({ onAddItems }) {
     </form>
   );
 }
-function PackingList({ items, onDeleteItem, onUpdateItem }) {
+function PackingList({ items, onDeleteItem, onUpdateItem, onDeleteList }) {
+  const [sort, setSort] = useState("input");
+
+  let sortedItems;
+
+  if (sort === "input") sortedItems = items;
+
+  if (sort === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description)); // use localComapre() to compare strings
+
+  if (sort === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -92,6 +113,15 @@ function PackingList({ items, onDeleteItem, onUpdateItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sort} onChange={(e) => setSort(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onDeleteList}>Clear List</button>
+      </div>
     </div>
   );
 }
@@ -134,3 +164,5 @@ function Stats({ items }) {
     </footer>
   );
 }
+
+// if we need to catch the value of select element to that use event.target.value
