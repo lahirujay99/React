09402b75d176b0095d76b7941bacbfaf6e25bdb0
lahirujay99 <lahirujayawardana94@gmail.com>
2023-06-23 23:29:26 +0530

    initial commit

---
+++ tip-calculator/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
+
+function TipCalculator() {
+  return (
+    <div>
+      <BillInput />
+      <SelectPercentage />
+      <SelectPercentage />
+      <Output />
+      <Reset />
+    </div>
+  );
+}
+function BillInput() {}
+function SelectPercentage() {}
+function Output() {}
+function Reset() {}
